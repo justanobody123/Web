255f2adcaa807805674edef7c2fe03949a324cd9
Кристина Волосова <justanobody017527@gmail.com>
2025-03-19 13:10:38 +0300

    Numeric field for delay DONE

--- 07-IntroductionToJS/switch-button.js
+++ 07-IntroductionToJS/switch-button.js
@@ -1,10 +1,11 @@
 // JavaScript source code
+let delay = 3;
 function switch_button()
 {
 	let switch_btn = document.getElementById("switch");
 	let image = switch_btn.src.split('/').at(-1);
 	console.log(image);
-	document.body.style.transition = "background-color 3s, color 3s";
+	document.body.style.transition = `background-color ${delay}s, color ${delay}s`;
 	document.body.style.backgroundColor = image === 'moon.png' ? '#323232' : 'white';
 	document.body.style.color = image === 'moon.png' ? 'white' : 'black';
 	switch_btn.style.transition = "opacity 0.5s";
@@ -16,4 +17,12 @@ function switch_button()
 	}, 500);
 
 	console.log(switch_btn.src);
+}
+function set_delay()
+{
+	let delay_id = document.getElementById("delay");
+	if (delay_id.value >= 0.1 && delay_id.value <=3)
+	{
+		delay = delay_id.value;
+	}
 }
